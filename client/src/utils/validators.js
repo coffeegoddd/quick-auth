0020e901128c982c0f825741b3bf3cfd4565a8a2
coffeegoddd <dbeaubrown@gmail.com>
2019-03-22 16:14:40 -0700

    style: moved validator functions to util folder

---
+++ client/src/utils/validators.js
@@ -0,0 +1,10 @@
+const validateEmail = (email) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+const validatePassword = (password) => {
+  const re = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+  return re.test(String(password));
+};
+
+module.exports = { validateEmail, validatePassword };
