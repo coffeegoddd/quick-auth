ebb7e6ee2c9ce76def81cd6c2bba86d95e12e1c3
coffeegoddd <dbeaubrown@gmail.com>
2019-03-23 10:58:24 -0700

    fix: fixed field validation so that it checks validity once a field is blurred

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -11,16 +11,20 @@ class Login extends Component {
       password: '',
       validEmail: false,
       validPassword: false,
-      submitClicked: false,
+      validUsername: false,
     };
     this.handleUsername = this.handleUsername.bind(this);
     this.handleEmail = this.handleEmail.bind(this);
     this.handlePassword = this.handlePassword.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.checkPassword = this.checkPassword.bind(this);
+    this.checkEmail = this.checkEmail.bind(this);
+    this.checkUsername = this.checkUsername.bind(this);
 
     this.msgMap = {
       email: 'please enter a valid email, like harry@potter.com',
-      password: 'passwords must > 6 chars, have a cap, a num, and a special char',
+      password: `pw must have minimum of > 8 chars, one uppercase letter, one lower
+      case, one number, one special char`,
     };
   }
 
@@ -47,27 +51,37 @@ class Login extends Component {
   }
 
   handleSubmit() {
-    this.setState((state) => {
-      const { username, email, password } = state;
-      const validEmail = validateEmail(email);
-      const validPassword = validatePassword(password);
-      if (!validEmail || ! validPassword) {
-        return {
-          validEmail,
-          validPassword,
-          submitClicked: true,
-        };
-      }
+    if (this.state.validUsername && this.state.validEmail && this.state.validPassword) {
+      const { username, email, password } = this.state;
       this.props.authenticate(username, email, password);
-      return {
+      this.setState({
         username: '',
         email: '',
         password: '',
         validEmail: false,
         validPassword: false,
-        submitClicked: false,
-      };
-    });
+      });
+    }
+  return;
+}
+
+  checkPassword(e) {
+    // Minimum eight characters, at least one uppercase letter, 
+    // one lowercase letter, one number and one special character:
+    const { value } = e.target;
+    const validPassword = validatePassword(value);
+    this.setState({ validPassword });
+  }
+
+  checkEmail(e) {
+    const { value } = e.target;
+    const validEmail = validateEmail(value);
+    this.setState({ validEmail });
+  }
+
+  checkUsername(e) {
+    if (e.target.value) return this.setState({ validUsername: true })
+    this.setState({ validUsername: false });
   }
 
   render() {
@@ -81,6 +95,7 @@ class Login extends Component {
             name="username"
             value={this.state.username}
             onChange={this.handleUsername}
+            onBlur={(e) => this.checkUsername(e)}
           />
         </div>
         <div>
@@ -90,9 +105,10 @@ class Login extends Component {
             name="email"
             value={this.state.email}
             onChange={this.handleEmail}
+            onBlur={(e) => this.checkEmail(e)}
           />
         </div>
-        {this.state.email && this.state.submitClicked && !this.state.validEmail ? <div>{this.msgMap['email']}</div> : null }
+        {this.state.email && !this.state.validEmail ? <div>{this.msgMap['email']}</div> : null }
         <div>
           <input
             type="password"
@@ -100,9 +116,10 @@ class Login extends Component {
             name="password"
             value={this.state.password}
             onChange={this.handlePassword}
+            onBlur={(e) => this.checkPassword(e)}
           />
         </div>
-        {this.state.password && this.state.submitClicked && !this.state.validPassword ? <div>{this.msgMap['password']}</div> : null }
+        {this.state.password && !this.state.validPassword ? <div>{this.msgMap['password']}</div> : null }
         <button onClick={this.handleSubmit}>submit</button>
       </div>
     );
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -11,16 +11,20 @@ class Signup extends Component {
       password: '',
       validEmail: false,
       validPassword: false,
-      submitClicked: false,
+      validUsername: false,
     };
     this.handleUsername = this.handleUsername.bind(this);
     this.handleEmail = this.handleEmail.bind(this);
     this.handlePassword = this.handlePassword.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.checkPassword = this.checkPassword.bind(this);
+    this.checkEmail = this.checkEmail.bind(this);
+    this.checkUsername = this.checkUsername.bind(this);
 
     this.msgMap = {
       email: 'please enter a valid email, like harry@potter.com',
-      password: 'passwords must > 6 chars, have a cap, a num, and a special char',
+      password: `pw must have minimum of > 8 chars, one uppercase letter, one lower
+      case, one number, one special char`,
     };
   }
 
@@ -46,27 +50,37 @@ class Signup extends Component {
   }
 
   handleSubmit() {
-    this.setState((state) => {
-      const { username, email, password } = state;
-      const validEmail = validateEmail(email);
-      const validPassword = validatePassword(password);
-      if (!validEmail || ! validPassword) {
-        return {
-          validEmail,
-          validPassword,
-          submitClicked: true,
-        };
+      if (this.state.validUsername && this.state.validEmail && this.state.validPassword) {
+        const { username, email, password } = this.state;
+        this.props.signup(username, email, password);
+        this.setState({
+          username: '',
+          email: '',
+          password: '',
+          validEmail: false,
+          validPassword: false,
+        });
       }
-      this.props.signup(username, email, password);
-      return {
-        username: '',
-        email: '',
-        password: '',
-        validEmail: false,
-        validPassword: false,
-        submitClicked: false,
-      };
-    });
+    return;
+  }
+
+  checkPassword(e) {
+    // Minimum eight characters, at least one uppercase letter, 
+    // one lowercase letter, one number and one special character:
+    const { value } = e.target;
+    const validPassword = validatePassword(value);
+    this.setState({ validPassword });
+  }
+
+  checkEmail(e) {
+    const { value } = e.target;
+    const validEmail = validateEmail(value);
+    this.setState({ validEmail });
+  }
+
+  checkUsername(e) {
+    if (e.target.value) return this.setState({ validUsername: true })
+    this.setState({ validUsername: false });
   }
 
   render() {
@@ -80,6 +94,7 @@ class Signup extends Component {
             name="username"
             value={this.state.username}
             onChange={this.handleUsername}
+            onBlur={(e) => this.checkUsername(e)}
           />
         </div>
         <div>
@@ -89,9 +104,10 @@ class Signup extends Component {
             name="email"
             value={this.state.email}
             onChange={this.handleEmail}
+            onBlur={(e) => this.checkEmail(e)}
           />
         </div>
-        {this.state.email && this.state.submitClicked && !this.state.validEmail ? <div>{this.msgMap['email']}</div> : null }
+        {this.state.email && !this.state.validEmail ? <div>{this.msgMap['email']}</div> : null }
         <div>
           <input
             type="password"
@@ -99,9 +115,10 @@ class Signup extends Component {
             name="password"
             value={this.state.password}
             onChange={this.handlePassword}
+            onBlur={(e) => this.checkPassword(e)}
           />
         </div>
-        {this.state.password && this.state.submitClicked && !this.state.validPassword ? <div>{this.msgMap['password']}</div> : null }
+        {this.state.password && !this.state.validPassword ? <div>{this.msgMap['password']}</div> : null }
         <button onClick={this.handleSubmit}>submit</button>
       </div>
     );
