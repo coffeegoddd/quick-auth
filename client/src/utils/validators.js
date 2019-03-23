71472206439c3d88888b6bdf85000fc4ccfda0f5
coffeegoddd <dbeaubrown@gmail.com>
2019-03-23 10:57:28 -0700

    fix: changed regex for password validation so it is clear what is required

--- client/src/utils/validators.js
+++ client/src/utils/validators.js
@@ -3,7 +3,9 @@ const validateEmail = (email) => {
   return re.test(String(email).toLowerCase());
 };
 const validatePassword = (password) => {
-  const re = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+  // Minimum eight characters, at least one uppercase letter, 
+  // one lowercase letter, one number and one special character:
+  const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
   return re.test(String(password));
 };
 
